7e921ab708d34459b59e93f7da83b799e1048775
getcmdrolled <wetturtle45@gmail.com>
2026-08-01 08:51:46 -0400

    refactor: migrate to mojmap

--- src/main/java/net/scoobis/svctts/SendTtsScreen.java
+++ src/main/java/net/scoobis/svctts/SendTtsScreen.java
@@ -1,33 +1,33 @@
 package net.scoobis.svctts;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.text.Text;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 
 public class SendTtsScreen extends Screen {
-    public SendTtsScreen(Text title) {
+    public SendTtsScreen(Component title) {
         super(title);
     }
 
-    public ButtonWidget sendButton;
-    public ButtonWidget exitButton;
-    public TextFieldWidget messageField;
+    public Button sendButton;
+    public Button exitButton;
+    public EditBox messageField;
 
     @Override
     protected void init() {
-        messageField = new TextFieldWidget(textRenderer, 10, height / 2 - 10, width - 20, 20, Text.literal("very real text that will definitely show up in game"));
+        messageField = new EditBox(font, 10, height / 2 - 10, width - 20, 20, Component.literal("very real text that will definitely show up in game"));
         messageField.setMaxLength(128);
         setFocused(messageField);
 
-        sendButton = ButtonWidget.builder(Text.translatable("button.svctts.send"), this::send).dimensions(width / 2 - 50, height / 2 + 10, 100, 20).build();
-        exitButton = ButtonWidget.builder(Text.literal("X"), button -> MinecraftClient.getInstance().setScreen(null)).dimensions(width - 30, 10, 20, 20).build();
+        sendButton = Button.builder(Component.translatable("button.svctts.send"), this::send).bounds(width / 2 - 50, height / 2 + 10, 100, 20).build();
+        exitButton = Button.builder(Component.literal("X"), button -> Minecraft.getInstance().setScreen(null)).bounds(width - 30, 10, 20, 20).build();
 
-        addDrawableChild(messageField);
-        addDrawableChild(sendButton);
-        addDrawableChild(exitButton);
+        addRenderableWidget(messageField);
+        addRenderableWidget(sendButton);
+        addRenderableWidget(exitButton);
     }
 
     @Override
@@ -36,17 +36,17 @@ public class SendTtsScreen extends Screen {
         return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
-    private void send(ButtonWidget buttonWidget) {
-        String text = messageField.getText();
-        messageField.setText("");
-        MinecraftClient.getInstance().setScreen(null);
+    private void send(Button buttonWidget) {
+        String text = messageField.getValue();
+        messageField.setValue("");
+        Minecraft.getInstance().setScreen(null);
         SvcTtsMod.addToQueue(text);
     }
 
     private void send() {
-        String text = messageField.getText();
-        messageField.setText("");
-        MinecraftClient.getInstance().setScreen(null);
+        String text = messageField.getValue();
+        messageField.setValue("");
+        Minecraft.getInstance().setScreen(null);
         if (!text.isEmpty()) {
             SvcTtsMod.addToQueue(text);
         }
--- src/main/java/net/scoobis/svctts/SvcTtsClient.java
+++ src/main/java/net/scoobis/svctts/SvcTtsClient.java
@@ -1,29 +1,29 @@
 package net.scoobis.svctts;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 
 public class SvcTtsClient implements ClientModInitializer {
-    private static KeyBinding openTtsScreenKeybind;
+    private static KeyMapping openTtsScreenKeybind;
 
     @Override
     public void onInitializeClient() {
-        openTtsScreenKeybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        openTtsScreenKeybind = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.svctts.openscreen",
-                InputUtil.Type.KEYSYM,
+                InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_B,
                 "key.svctts.category"
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (openTtsScreenKeybind.wasPressed()) {
-                MinecraftClient.getInstance().setScreen(new SendTtsScreen(Text.literal("yo")));
+            while (openTtsScreenKeybind.consumeClick()) {
+                Minecraft.getInstance().setScreen(new SendTtsScreen(Component.literal("yo")));
             }
         });
     }
